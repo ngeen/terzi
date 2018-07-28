67b20a594f6ac7530e7f193f7a252bfe68d76d48
engin <o.enginoz@gmail.com>
2018-07-29 00:29:05 +0300

    düzenleme yapıldı

--- src/main/java/com/oz/tailor/controller/utils/UserController.java
+++ src/main/java/com/oz/tailor/controller/utils/UserController.java
@@ -1,15 +1,7 @@
 package com.oz.tailor.controller.utils;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.oz.tailor.model.User;
+import com.oz.tailor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.oz.tailor.model.User;
-import com.oz.tailor.repository.UserRepository;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -68,19 +60,16 @@ public class UserController {
     }
 	
     @PostMapping("/changePass")
-    public String singleFileUpload(@RequestBody String pass,RedirectAttributes redirectAttributes) throws IOException {
+    public ResponseEntity<?> singleFileUpload(@RequestBody Map<String, Object> pass) {
 
         User user = userController.getAuthUser();
 		
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		user.setPassword(bCryptPasswordEncoder.encode(pass));
+        user.setPassword(bCryptPasswordEncoder.encode(pass.get("password").toString()));
 		
 		userRepository.save(user);
-		
-		redirectAttributes.addFlashAttribute("message",
-		        "Profil Fotoğrafınız Başarıyla Güncellenmiştir");
 
-        return "redirect:/uploadImage";
+        return new ResponseEntity<String>("{\"result\":\"success\"}", HttpStatus.CREATED);
     }
  
     // ------------------- Delete a User-----------------------------------------
